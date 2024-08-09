8509e6a32d96896da9a29eb6d13c609897d8caf2
Minchuk Ilya <cacatubutyt@gmail.com>
2024-08-09 11:15:22 +0200

    feat: create ROUTES and USER_AUTH_ROUTES constants

---
+++ src/utils/constants/routes.ts
@@ -0,0 +1,40 @@
+export const ROUTES = {
+  ROOT: {
+    id: 'home',
+    name: 'home',
+    path: '/',
+  },
+  MENU: {
+    id: 'menu',
+    name: 'menu',
+    path: '/menu',
+  },
+  SIGN_IN: {
+    id: 'sign in',
+    name: 'sign in',
+    path: '/sign-in',
+  },
+  SIGN_UP: {
+    id: 'sign up',
+    name: 'sign up',
+    path: '/sign-up',
+  },
+};
+
+export const AUTH_ROUTES = {
+  ROOT: {
+    id: 'home',
+    name: 'home',
+    path: '/',
+  },
+  MENU: {
+    id: 'menu',
+    name: 'menu',
+    path: '/menu',
+  },
+  USER_PROFILE: {
+    id: 'profile',
+    name: 'profile',
+    path: '/profile',
+  },
+};

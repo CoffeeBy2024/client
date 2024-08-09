7227e3787f60e68d232e3c5753aad69086d981b0
Minchuk Ilya <cacatubutyt@gmail.com>
2024-08-09 12:36:48 +0200

    feat: add sign-up page

---
+++ src/modules/SignUpView/SignUpView.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+
+import { Container, Typography } from '@/components';
+import { ROUTES } from '@/utils/constants';
+
+const TEXT = {
+  H1: 'sign up page',
+  SIGN_IN_LINK: 'sign in',
+};
+
+const SignUpView = () => {
+  return (
+    <section className="h-full">
+      <Container>
+        <Typography tag="h1" variant="title-1">
+          {TEXT.H1}
+        </Typography>
+        <Link href={ROUTES.SIGN_IN.path} className="bg-orange-300">
+          {TEXT.SIGN_IN_LINK}
+        </Link>
+      </Container>
+    </section>
+  );
+};
+
+export { SignUpView };
--- src/modules/index.ts
+++ src/modules/index.ts
@@ -2,3 +2,4 @@ export * from './HomeView/HomeView';
 export * from './NotFoundView/NotFoundView';
 export * from './MenuView/MenuView';
 export * from './SignInView/SignInView';
+export * from './SignUpView/SignUpView';

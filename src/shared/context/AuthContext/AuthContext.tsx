46851dbca469406e6378f80792a9e0bddf6f1295
Minchuk Ilya <cacatubutyt@gmail.com>
2024-09-25 13:57:27 +0200

    feat: upgarde auth context to handle tokens auth logic

--- src/shared/context/AuthContext/AuthContext.tsx
+++ src/shared/context/AuthContext/AuthContext.tsx
@@ -1,8 +1,20 @@
 import { createContext } from 'react';
 
+import { AuthService } from '@/shared/api';
+
+export type RegisterUserReturn = ReturnType<typeof AuthService.register>;
+export type LoginUserReturn = ReturnType<typeof AuthService.login>;
+export type LogoutUserReturn = ReturnType<typeof AuthService.logout>;
+
 interface IAuthContextData {
   isAuthenticated: boolean;
   setIsAuthenticated: (isLoggedIn: boolean) => void;
+  registerUser: (dto: RegisterUserDto) => Promise<RegisterUserReturn>;
+  user: User | null;
+  setUser: (user: User) => void;
+  loginUser: (dto: LoginUserDto) => Promise<LoginUserReturn>;
+  logoutUser: () => Promise<LogoutUserReturn>;
+  isFirstLoad: boolean;
 }
 
 const AuthContext = createContext<IAuthContextData | undefined>(undefined);
--- src/shared/context/AuthContext/AuthProvider.tsx
+++ src/shared/context/AuthContext/AuthProvider.tsx
@@ -1,27 +1,99 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React from 'react';
+
+import { AuthService } from '@/shared/api';
 
 import { AuthContext } from './AuthContext';
+import type {
+  LoginUserReturn,
+  LogoutUserReturn,
+  RegisterUserReturn,
+} from './AuthContext';
 
 interface IAuthProviderProps {
   children: React.ReactNode;
-  initialAuthState: boolean;
 }
 
-const AuthProvider: React.FC<IAuthProviderProps> = ({
-  children,
-  initialAuthState,
-}) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(initialAuthState);
+const AuthProvider: React.FC<IAuthProviderProps> = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = React.useState(false);
+  const [user, setUser] = React.useState<User | null>(null);
+  const [isFirstLoad, setIsFirstLoad] = React.useState<boolean>(true);
+
+  const checkAuth = async () => {
+    try {
+      const response = await AuthService.refreshTokens();
+      localStorage.setItem('accessToken', response.data.accessToken);
+      setIsAuthenticated(true);
+      setUser(response.data.user);
+      return response;
+    } catch (err) {
+      if (axios.isAxiosError(err)) {
+        console.error('Axios Error:', err);
+      } else {
+        console.error('Unexpected Error:', err);
+      }
+    } finally {
+      setIsFirstLoad(false);
+    }
+  };
+
+  React.useEffect(() => {
+    checkAuth();
+  }, []);
+
+  const registerUser = React.useCallback(
+    async (dto: RegisterUserDto): Promise<RegisterUserReturn> => {
+      const response = await AuthService.register(dto);
+      setUser(response.data);
+      return response;
+    },
+    []
+  );
+
+  const loginUser = React.useCallback(
+    async (dto: LoginUserDto): Promise<LoginUserReturn> => {
+      const response = await AuthService.login(dto);
+      localStorage.setItem('accessToken', response.data.accessToken);
+      setIsAuthenticated(true);
+      setUser(response.data.user);
+      return response;
+    },
+    []
+  );
+
+  const logoutUser = React.useCallback(async (): Promise<LogoutUserReturn> => {
+    const response = await AuthService.logout();
+    setIsAuthenticated(false);
+    localStorage.removeItem('accessToken');
+    setUser(null);
+    return response;
+  }, []);
+
+  const contextValue = React.useMemo(
+    () => ({
+      isAuthenticated,
+      setIsAuthenticated,
+      registerUser,
+      user,
+      setUser,
+      loginUser,
+      logoutUser,
+      isFirstLoad,
+    }),
+    [
+      isAuthenticated,
+      setIsAuthenticated,
+      registerUser,
+      user,
+      setUser,
+      loginUser,
+      logoutUser,
+      isFirstLoad,
+    ]
+  );
 
   return (
-    <AuthContext.Provider
-      value={{
-        isAuthenticated,
-        setIsAuthenticated,
-      }}
-    >
-      {children}
-    </AuthContext.Provider>
+    <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
   );
 };
 

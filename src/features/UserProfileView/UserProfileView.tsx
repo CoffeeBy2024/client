fb06569e72081c7e539335ea05e87c18557bd14d
Minchuk Ilya <cacatubutyt@gmail.com>
2024-09-25 14:48:24 +0200

    feat: add logout button in UserProfileView.tsx

--- src/features/UserProfileView/UserProfileView.tsx
+++ src/features/UserProfileView/UserProfileView.tsx
@@ -1,14 +1,39 @@
-import { Typography } from '@/shared/ui';
+import axios from 'axios';
+import { useRouter } from 'next/router';
+
+import { Button, Container, Typography } from '@/shared/ui';
+import { useAuth } from '@/utils/hooks';
 
 const TEXT = {
   H1: 'user profile page',
+  BTN_LOGOUT: 'logout',
 };
 
 const UserProfileView = () => {
+  const { logoutUser } = useAuth();
+  const router = useRouter();
+
+  const onLogoutClick = async () => {
+    try {
+      await logoutUser();
+      router.push('/');
+    } catch (err) {
+      if (axios.isAxiosError(err)) {
+        console.error('Status:', err.status);
+        console.error('Message:', err.message);
+      }
+    }
+  };
+
   return (
-    <Typography tag="h1" variant="title-1">
-      {TEXT.H1}
-    </Typography>
+    <Container>
+      <Typography tag="h1" variant="title-1">
+        {TEXT.H1}
+      </Typography>
+      <Button variant="contained" size="medium" onClick={onLogoutClick}>
+        {TEXT.BTN_LOGOUT}
+      </Button>
+    </Container>
   );
 };
 
